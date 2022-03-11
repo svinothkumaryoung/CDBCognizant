7e54c12f4c86f0058af862edfe5b40b9acfb0d58
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-03-11 09:46:34 +0530

    Initial commit

---
+++ src/Registeration.java
@@ -0,0 +1,15 @@
+public class Registeration {
+    int registeration=10101;
+    String name="vinothkumar selvaarasan";
+void printData()
+{
+    System.out.println("Registeration No "+registeration);
+    System.out.println("Name is "+name);
+}
+
+    public static void main(String[] args) {
+        Registeration regi=new Registeration();
+        regi.printData();
+    }
+
+}
