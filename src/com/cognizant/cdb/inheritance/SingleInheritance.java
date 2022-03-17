2e6a458f2cc14c45c0308430a49476cbbe9ab3fb
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-03-17 10:50:00 +0530

    Get Value from user

---
+++ src/com/cognizant/cdb/inheritance/SingleInheritance.java
@@ -0,0 +1,27 @@
+package com.cognizant.cdb.inheritance;
+class parentClass{
+    int registerNo=6666;
+    String name="Vinothkumar Selvaarasan";
+    void printdata()
+    {
+        System.out.println("RegisterNo is "+registerNo);
+        System.out.println("Name is "+name);
+    }
+}
+
+
+public class SingleInheritance extends parentClass {
+    String collegeName="SIET";
+    void printCollegeData()
+    {
+        System.out.println("College Name "+collegeName);
+    }
+
+    public static void main(String[] args) {
+        parentClass pc=new parentClass();
+        pc.printdata();
+        SingleInheritance sinherit=new SingleInheritance();
+        sinherit.printdata();
+        sinherit.printCollegeData();
+    }
+}
